f35c3350c61a22db31a15ba589cc4c7c05e242e7
Ederson Machado <edersonmberti@gmail.com>
2021-09-16 22:41:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,159 @@
+const fs = require('fs')
+const readline = require('readline');
+
+let PC = 0
+const codes = []
+const R = Array(32).fill(0)
+
+const codeTest = "addi $t1, $t0, 1"
+// ADD=1 , ADDI, SUB, SUBI, BEQ, B, 
+
+const NOP = "nop"
+const ADD = "add"
+const ADDI = "addi"
+const SUB = "sub"
+const SUBI = "subi"
+const BEQ = "beq"
+const B = "b"
+
+const defaultRegisterValues = {
+    opCode: NOP,
+    op1: 0,
+    op2: 0,
+    op3: 0,
+    valid: 0,
+    tempValue1: 0,
+    tempValue2: 0,
+    tempValue3: 0,
+}
+
+const operationCodes = {
+    "add": (register) => { register.tempValue1 = register.tempValue2 + register.tempValue3 },
+    "addi": (register) => { register.tempValue1 = register.tempValue2 + register.tempValue3 },
+    "sub": (register) => { register.tempValue1 = register.tempValue2 - register.tempValue3 },
+    "subi": (register) => { register.tempValue1 = register.tempValue2 - register.tempValue3 },
+    "beq": (register) => { },
+    "b": (register) => { },
+    "nop": (register) => { },
+}
+
+let registerFetch = defaultRegisterValues
+let registerDecode = defaultRegisterValues
+let registerExecute = defaultRegisterValues
+let registerMemory = defaultRegisterValues
+let registerWriteBack = defaultRegisterValues
+
+const fetch = () => {
+    const code = codes[PC]
+
+    const formatedCode = code.toLowerCase().replace(",", "").replace(",", "")
+
+    if (formatedCode === NOP) {
+        registerFetch = defaultRegisterValues
+    } else {
+        const codeList = formatedCode.split(" ")
+        registerFetch.opCode = codeList[0]
+
+        if (codeList.length > 3) {
+            registerFetch.op1 = Number(codeList[2].match(/\d/)[0])
+            registerFetch.op2 = Number(codeList[3].match(/\d/)[0])
+            registerFetch.op3 = Number(codeList[1].match(/\d/)[0])
+        } else {
+            registerFetch.op1 = Number(codeList[1].match(/\d/)[0])
+            registerFetch.op2 = Number(codeList[2].match(/\d/)[0])
+            registerFetch.op3 = 0
+        }
+    }
+
+    console.log("Register Fetch", registerFetch)
+
+    PC += 1
+}
+
+const decode = () => {
+    registerDecode = registerFetch
+
+    if (registerDecode.opCode === B)
+        registerDecode.tempValue1 = registerDecode.op1;
+    else {
+        registerDecode.tempValue2 = R[registerDecode.op2];
+        if ((registerDecode.opCode === ADDI) || (registerDecode.opCode === SUBI))
+            registerDecode.tempValue3 = registerDecode.op3;
+        else
+            registerDecode.tempValue3 = R[registerDecode.op3];
+    }
+
+    console.log("Register Decode", registerDecode)
+}
+
+const execute = () => {
+    registerExecute = registerDecode
+
+    operationCodes[registerExecute.opCode](registerExecute)
+
+    console.log("Register Execute", registerExecute)
+}
+
+const memory = () => {
+    registerMemory = registerExecute
+}
+
+const writeBack = () => {
+    registerWriteBack = registerMemory
+
+    if ((registerWriteBack.opCode == ADDI) || (registerWriteBack.opCode == ADD) || (registerWriteBack.opCode == SUBI) || (registerWriteBack.opCode == SUB))
+        R[registerWriteBack.op1] = registerWriteBack.tempValue1;
+
+    console.log("Register WriteBack", registerWriteBack)
+}
+
+const loadCode = () => {
+    const filename = "code.txt"
+
+    const readInterface = readline.createInterface({
+        input: fs.createReadStream(filename),
+        console: false
+    });
+
+    readInterface.on('line', (line) => {
+        codes.push(line)
+    }).on("close", () => { })
+}
+
+const runNextLine = () => {
+    console.log(`--------------------------------------------- Code: ${codes[PC]} Line: ${PC} ------------------------------------------------`)
+
+    writeBack()
+    memory()
+    execute()
+    decode()
+    fetch()
+
+    console.log("R", R)
+}
+
+loadCode()
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const runner = async () => {
+    loadCode()
+
+    await sleep(500)
+
+    runNextLine()
+
+    await sleep(500)
+
+    runNextLine()
+
+    await sleep(500)
+
+    runNextLine()
+
+    await sleep(500)
+}
+
+runner()
